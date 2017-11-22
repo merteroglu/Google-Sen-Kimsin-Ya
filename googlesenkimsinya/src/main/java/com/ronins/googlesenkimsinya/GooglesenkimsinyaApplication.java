3595764061215517f0f5653e53e72955ac7c4401
Mert Erogu <merteroglu16@gmail.com>
2017-11-22 14:03:31 +0300

    v0
    - Boş proje eklendi
    - Maven ayarlandı

---
+++ googlesenkimsinya/src/main/java/com/ronins/googlesenkimsinya/GooglesenkimsinyaApplication.java
@@ -0,0 +1,12 @@
+package com.ronins.googlesenkimsinya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglesenkimsinyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglesenkimsinyaApplication.class, args);
+	}
+}
